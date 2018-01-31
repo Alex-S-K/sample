bbac4f6e5a04e829c49ab055a23614109e024608
Alex <alexsunkang@163.com>
2018-01-31 23:34:37 +0000

    added Layout style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,28 @@
     <head>
         <meta charset="utf-8">
         <title>@yield('title', 'Sample') - Alex's App</title>
+        {{-- Css link --}}
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        @yield('content')
+        {{-- Navbar --}}
+        <header class="navbar navbar-fixed-top navbar-inverse">
+            <div class="container">
+                <div class="col-md-10 col-md-offset-1">
+                    <a href="/" id="logo">Sample App</a>
+                    <nav>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="/help">Help</a></li>
+                            <li><a href="#">Sign Up</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
+        {{-- Content --}}
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
+
 @section('title', 'Home')
+
 @section('content')
-    <h1>Home Page</h1>
+    <div class="jumbotron">
+        <h1>Hello, world!</h1>
+        <p class="lead">Everything, Well be start here</p>
+        <p>
+            <a href="#" role="button" class="btn btn-success btn-lg">Sign in</a>
+        </p>
+    </div>
 @endsection
